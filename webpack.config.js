4933af4c5a8f307c417ffbc8d6f2659104c6498b
yangjinkai <yangjinkai@yangjinkaideMacBook-Pro.local>
2019-11-16 16:27:01 +0800

    vue dep

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,12 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/lib/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'dtree-table.js'
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'dtree-table.js',
+    library: 'dtree-table', // 指定的就是你使用require时的模块名
+    libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
   module: {
     rules: [
